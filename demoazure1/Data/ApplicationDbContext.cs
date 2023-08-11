1eaf3f654e581509f27c74547633fb41f6e45108
shwetha k <shwethakulk143@gmail.com>
2023-08-11 11:27:31 +0530

    Add project files.

---
+++ demoazure1/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using demoazure1.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace demoazure1.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+        public DbSet<LeadEntity> Leads { get; set; }
+
+    }
+}
---
+++ demoazure1/Models/LeadEntity.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Runtime.CompilerServices;
+
+namespace demoazure1.Models
+{
+    public class LeadEntity
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [DataType(DataType.Date)]
+        [DisplayName("Date")]
+        [Required]
+        
+        public DateTime LeadDate { get; set; }
+
+        [DisplayName("Lead Source")]
+        [Required]
+        [StringLength(50)]
+        public string LeadSource { get; set; }
+        [Required]
+        [StringLength(10)]
+        public string Name { get; set; }
+       
+        [Required(ErrorMessage = "Phone Number Required!")]
+        [RegularExpression(@"^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$",
+        ErrorMessage = "Entered phone format is not valid.")]
+        public string mobile { get; set; }
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+    }
+}
